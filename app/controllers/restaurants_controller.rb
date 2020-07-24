24b55258228613f2062e6354f1a5e1c1c447b51e
vagrant <vagrant@localhost.localdomain>
2020-07-24 10:14:53 +0000

    不具合修正

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :top]
 
   PER = 16
 
